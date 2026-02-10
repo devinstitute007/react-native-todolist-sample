ef9394639622f6b03187806fc7c5d446412e6c90
devinstitute007 <cjiogo@gmail.com>
2026-02-10 11:47:41 +0100

    react-native-introduction_v1.0.0

--- App.style.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const s = StyleSheet.create({
-  root: {
-    padding: 10,
-    flex: 1,
-  },
-});
--- constants.js
+++
@@ -1,9 +0,0 @@
-export const UNITS = {
-  celcius: "°C",
-  faranheit: "°F",
-};
-
-export const UNIT_LABELS = {
-  [UNITS.celcius]: "Celcius",
-  [UNITS.faranheit]: "Faranheit",
-};
